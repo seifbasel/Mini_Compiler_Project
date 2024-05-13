1be215005c34ca3747c84e9fe5331d523ae1aa33
seif basel <seifbasel950@gmail.com>
2024-05-14 01:09:09 +0300

    update

--- test/sympol_table.py
+++ test/sympol_table.py
@@ -10,7 +10,6 @@ tokens = [
     ('space', r'\s'),
 ]
 
-
 def lexer(code):
     pos = 0
     while pos < len(code):
@@ -28,7 +27,7 @@ def unordered_symbol_table(code):
     symbol_table = {}
     for token_type, value in lexer(code):
         if token_type == 'int':
-            data_type = value
+            data_type = value   
         elif token_type == 'id':
             identifier = value
             if identifier not in symbol_table:
@@ -36,22 +35,16 @@ def unordered_symbol_table(code):
                     'type': data_type,
                     'value': None,
                 }
-        elif token_type == 'assign':
-            assignment_mode = True
-        elif token_type == 'number' and assignment_mode:
+        elif token_type == 'number' :
             symbol_table[identifier]['value'] = value
-            assignment_mode = False
-        elif token_type == 'id' and not assignment_mode:
-            if value in symbol_table:
-                symbol_table[value]['signal_lines']
 
     return symbol_table
 
-
 # Example usage
 code = '''
-int a = 5;
-int b = 3;
+int a = 1;
+int b = 2;
+int c= 3;
 '''
 
 # Get symbol table
--- try.py
+++ try.py
@@ -1,37 +1,37 @@
 import re
-tokens=[
-    ('number',r'\d+'),
-    ('int',r'int'),
-    ('id',r'[a-z]'),
-    ('assign',r'\='),
-    ('semicolon',r'\;'),
-    ('newline',r'\n'),
-    ('space',r'\s'),
-    ]
-
 
 def lexer(code):
-    pos=0
-    while pos<len(code):
-        for  tokentybe,pattern in tokens:
-            match=re.match(pattern,code[pos:])
-            if match:
-                value=match.group()
-                if tokentybe !='newline' and tokentybe !='space':
-                    yield(value,tokentybe)
-                pos+=match.end()
-                break
-                
-# test code
+    tokens = [
+        ('number', r'\d+'),
+        ('assign', r'='),
+        ('int', r'int'),
+        ('id', r'[a-z]'),
+        ('semicolon', r';'),
+    ]
+    for token_type, pattern in tokens:
+        for match in re.finditer(pattern, code):
+            yield (token_type, match.group())
+
+def symbol_table(code):
+    table = {}
+    data_type = None
+    identifier = None
+    for token_type, value in lexer(code):
+        if token_type == 'int':
+            data_type = value
+        elif token_type == 'id':
+            identifier = value
+            table[identifier] = {'type': data_type, 'value': None}
+        elif token_type == 'assign':
+            pass  # No need to do anything here
+        elif token_type == 'number' and identifier is not None:
+            table[identifier]['value'] = value
+    return table
+
 code = '''
 int a = 5;
 int b = 3;
 '''
-
-# Get the tokens
-tokens_list = list(lexer(code))
-
-# Print the tokens
-print("Tokens:")
-for token in tokens_list:
-    print(token)
+print("Symbol Table:")
+for identifier, details in symbol_table(code).items():
+    print(f"Identifier: {identifier}, Type: {details['type']}, Value: {details['value']}")
